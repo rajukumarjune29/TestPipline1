496fa4cf774826456b19222e1bc2b6c8cedace26
Raju Kumar <rajukaumarsahu.29@gmail.com>
2023-07-18 16:54:29 +0530

    new project

---
+++ src/main/java/com/techwave/TestSpring3Application.java
@@ -0,0 +1,13 @@
+package com.techwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpring3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/techwave/TestSpring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
